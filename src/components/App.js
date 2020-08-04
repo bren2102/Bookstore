2d0a1d2bb4b647876a917f6e5b07d9e091fe23df
bren2102 <brb.yg.21@gmail.com>
2020-08-04 15:29:20 -0500

    Style header

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,24 @@ import React from 'react';
 import BooksList from '../containers/bookslist';
 import BooksForm from '../containers/booksform';
 
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
 class App extends React.Component {
   render() {
     return (
       <div id="App">
+        <header>
+          <div>
+            <h1>Bookstore CMS</h1>
+            <ul>
+              <li>BOOKS</li>
+              <li>CATEGORY</li>
+            </ul>
+          </div>
+          <span><FontAwesomeIcon icon={faUser} id="profileImg" /></span>
+        </header>
         <BooksList />
         <BooksForm />
       </div>
