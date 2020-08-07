a9cb3484fb5f2c9356440553b3ea6c1c588f1a60
bren2102 <brb.yg.21@gmail.com>
2020-08-07 12:33:44 -0500

    Remove credentials

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyB7ui41xBfpIHW3kEZMxnce-WDyV4m6stw',
-  authDomain: 'bookstore-36ea1.firebaseapp.com',
-  databaseURL: 'https://bookstore-36ea1.firebaseio.com',
-  projectId: 'bookstore-36ea1',
-  storageBucket: 'bookstore-36ea1.appspot.com',
-  messagingSenderId: '44707957004',
-  appId: '1:44707957004:web:f75230c895edc3bec0e129',
+  apiKey: 'PASTE YOUR KEY HERE',
+  authDomain: 'PASTE YOUR KEY HERE',
+  databaseURL: 'PASTE YOUR KEY HERE',
+  projectId: 'PASTE YOUR KEY HERE',
+  storageBucket: 'PASTE YOUR KEY HERE',
+  messagingSenderId: 'PASTE YOUR KEY HERE',
+  appId: 'PASTE YOUR KEY HERE',
 };
 firebase.initializeApp(config);
 export const db = firebase.database().ref();
